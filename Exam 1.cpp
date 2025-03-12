007885feb4ae2831d8d9709bd1ba37a746aedc6a
Brenna Meyer <brennameyer9@gmail.com>
2025-03-12 11:05:38 -0400

    Add project files.

---
+++ Exam 1.cpp
@@ -0,0 +1,12 @@
+// Exam 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Programmer:Brenna Meyer
+// Date: March, 12, 2025
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
